b07bb98fd1a59e158cfd92a1e8e3beab125b92e3
MMunoz713 <odst.mrtn@gmail.com>
2020-02-29 13:55:43 -0600

    wrote out workout.js file

---
+++ models/workout.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter a workout name"
+    },
+    sets: {
+        type: Number,
+        required: "How many sets?"
+    },
+    reps: {
+        type: Number,
+        required: "How many reps?"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Workout = mongoose.model("Workout",workoutSchema);
+
+module.exports = Workout;
